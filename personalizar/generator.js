a9d7759f79b4c4048ccc45a9b49d4f87bb8d295d
CsarMan <cesarpaulangelo@gmail.com>
2019-05-30 23:26:42 -0400

    This fixes #34 and others
    - Ahora al restablecer la generadora estando en el semestre 20, se rehabilita la opción para avanzar de semestre
    - Se incluyó al una "/" al final de los botones que dirigen a otras características

--- personalizar/generator.js
+++ personalizar/generator.js
@@ -36,6 +36,30 @@ function start_generator() {
 
 function generate() {
     saveSemester();
+    // Verificacion ramos duplicados
+    let ramosCursados = new Set()
+    let sem = 0
+    for (var s in ramosSemestres) {
+        sem++;
+        let ramosDuplicados = [];
+        ramosSemestres[s].forEach(ramo => {
+            if (ramosCursados.has(ramo)) {
+                ramosDuplicados.push(ramo)
+            } else {
+                ramosCursados.add(ramo)
+            }
+        });
+        ramosDuplicados.forEach(ramo => {
+            let i = ramosSemestres[s].indexOf(ramo);
+            ramosSemestres[s].splice(i,1)
+        })
+    }
+    while (ramosSemestres[sem] == []) {
+        ramosSemestres[sem] = null;
+        sem--
+    }
+    saveSemester()
+
     window.location.href = "malla.html?m=" + current_malla;
 }
 
@@ -141,6 +165,7 @@ function limpiarCalculadora() {
     semestre = 1
     d3.select('#back').attr('disabled', 'disabled');
     d3.select('#semestre').text(semestre);
+    d3.select('#forward').attr('disabled',null);
     let customSiglas = Array.from(custom_ramos.values());
     customSiglas.forEach(sigla => {
         borrarRamo(sigla);
