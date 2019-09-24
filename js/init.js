fb364ca58790c78955a16e4dd4773cdfed6d8257
Franco Sanllehi <f.sanllehiromero@uandresbello.edu>
2019-09-24 08:11:33 -0300

    More improvements -Fixed some typos -Moved all JS files to /JS/ -Created new way to load "Carreras" on Nav -Templates system! (for code reuse) -Created init.js for reduce recursive code -New way to fetch JS files in parallel and load them asynchronously -Formatted "Prioridad" index.html -Improved "Prioridad" index.html --Script and Style Tags --Changed Nav to NavBar -Fixed .gitignore (sort of)

---
+++ js/init.js
@@ -0,0 +1,45 @@
+/**
+ * Obtencion de archivos JS de manera paralela y carga sincronica
+ * @author Franco Sanllehi <f.sanllehiromero@uandresbello.edu>
+ */
+loadjs(['https://kit.fontawesome.com/bf671ef02a.js', 'https://cdn.jsdelivr.net/npm/sweetalert2@8', 'https://cdnjs.cloudflare.com/ajax/libs/d3/4.13.0/d3.min.js', 'js/ramos.js', 'js/selectableRamo.js', 'js/calculator.js', 'js/canvas.js'], 'init');
+loadjs.ready('init', function () {
+    console.log('Recursos cargados!');
+});
+
+function assureMail() {
+    Swal.fire({
+        title: '¿Estas seguro?',
+        text: "Para hacer cambios en alguna malla, primero te invito a mirar el codigo fuente ¡Allí podras encontrar toda la informacion que necesitas!",
+        type: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Contacto Mail'
+    }).then((result) => {
+        if (result.value) {
+            Swal.fire(
+                'OK!',
+                'Gracias por ponerte en contacto con nosotros :)',
+                'success'
+            );
+            window.location.href = "mailto:sebastian.aedo@sansano.usm.cl";
+        }
+    })
+}
+
+function render(props) {
+    return function(tok, i) {
+        return (i % 2) ? props[tok] : tok;
+    };
+}
+
+$.getJSON('/data/carreras.json', function(data) {
+    $.each(data, function(index, value) {
+        let tabTpl = $('script[data-template="tab-template"]').text().split(/\${(.+?)}/g);
+        value = [value];
+        $('#carreras-nav').append(value.map(function (value) {
+            return tabTpl.map(render(value)).join('');
+        }));
+    });
+});
